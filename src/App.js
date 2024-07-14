1b94fe862b2c371587a248dda9d6bb6ca7f000dd
Kaylea <kayleapaup@yahoo.com>
2024-07-14 15:32:53 -0500

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather
+      Weather App
+      <footer>
+        This project was coded by Kaylea Paup and is{" "}
+        <a href="https://github.com/paupyk/finalweather" target="_blank">
+          open-soucred on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
