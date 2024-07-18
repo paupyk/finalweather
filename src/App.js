e754306fd34a34f09caf0aea2d1df5702882f54d
Kaylea <kayleapaup@yahoo.com>
2024-07-17 22:01:31 -0500

    Added API integration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React from 'react';
 import Weather from './Weather';
+import axios from "axios";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Los Angeles" />
       <footer>
         This project was coded by Kaylea Paup and is{" "}
         <a href="https://github.com/paupyk/finalweather" target="_blank">
--- src/Weather.js
+++ src/Weather.js
@@ -1,31 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
 export default function Weather() {
-    return (
-        <div className="Weather">
-            <form> 
-                <input type="search" placeholder="Enter a city.." className="form-control" />
-                <input type="submit" value="Search" className="btn btn-primary" />
-            </form>
-            <h1>Los Angeles</h1>
-            <ul>
-                <li>Sunday 4:20</li>
-                <li>Sunny</li>
-            </ul>
-            <div className="row">
-                <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="Sunny" />
-                    6℃
-                </div>
-                <div className="col-6">
-                    <ul>
-                        <li>Precipitation: 0%</li>
-                        <li>Humidity: 47%</li>
-                        <li>Wind: 9 mph</li>
-                    </ul>
+    const [weatherData, setWeatherData] = useState({ ready: false});
+    function handleResponse(response) {
+        console.log(response.data);
+        setWeatherData({
+            ready: true,
+            temperature: response.data.temperature.current,
+            wind: response.data.wind.speed,
+            humidity: response.data.temperature.humidity,
+            date: "Sunday 4:20",
+            description: response.data.condition.description,
+            city: response.data.city,
+            iconURL: "https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+        });
+    }
+    if (weatherData.ready) {
+        return (
+            <div className="Weather">
+                <form>
+                    <div className="row">
+                        <div className="col-9">
+                            <input type="search" placeholder="Enter a city.." className="form-control" autoFocus="on" />
+                        </div>
+                        <div className="col-3">
+                            <input type="submit" value="Search" className="btn btn-primary w-100" />
+                        </div>
+                    </div>
+                </form>
+                <h1>{weatherData.city}</h1>
+                <ul>
+                    <li>{weatherData.date}</li>
+                    <li className="text-capitalize">{weatherData.description}</li>
+                </ul>
+                <div className="row mt-3">
+                    <div className="col-6">
+
+                        <div className="clearfix">
+                            <img src={weatherData.iconURL} alt={weatherData.description} className="float-left" />
+                            <div className="float-left">
+                                <span className="temperature">
+                                    {Math.round(weatherData.temperature)}
+                                </span>
+                                <span className="unit">℃</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <ul>
+                            <li>Precipitation: 0%</li>
+                            <li>Humidity: {weatherData.humidity}%</li>
+                            <li>Wind: {weatherData.wind} mph</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        const apiKey = "dt1bb39f7643d6635aece74b30o7b493";
+        let city = "Los Angeles";
+        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+
+        return "Loading..."
+    }
 }
