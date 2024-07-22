beeed32115725a680841480ce761c344d2dc8197
Kaylea <kayleapaup@yahoo.com>
2024-07-22 11:09:03 -0500

    Updated Weather with new data and moved functions

--- src/Weather.js
+++ src/Weather.js
@@ -3,44 +3,64 @@ import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
 import axios from "axios";
 
-export default function Weather() {
-      const [weatherData, setWeatherData] = useState({ ready: false });
-    function handleResponse(response) {
-        console.log(response.data);
-        setWeatherData({
-                  ready: true,
-      temperature: response.data.temperature.current,
-            humidity: response.data.temperature.humidity,
-      date: new Date(response.data.time * 1000),
-      description: response.data.condition.description,
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
       wind: response.data.wind.speed,
-    city: response.data.city,
+      city: response.data.name,
     iconURL: "https://ssl.gstatic.com/onebox/weather/64/sunny.png"
         });
     }
-    if (weatherData.ready) {
-        return (
-            <div className="Weather">
-                <form>
-                    <div className="row">
-                        <div className="col-9">
-                            <input type="search" placeholder="Enter a city.." className="form-control" autoFocus="on" />
-                        </div>
-                        <div className="col-3">
-                            <input type="submit" value="Search" className="btn btn-primary w-100" />
-                        </div>
-                    </div>
-                </form>
-                <WeatherInfo data={weatherData} />
-            </div>
-        );
-    } else {
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
+    }
+
+    function handleCityChange(event) {
+    setCity(event.target.value);
+    }
+    
+        function search() {
         const apiKey = "dt1bb39f7643d6635aece74b30o7b493";
-        let city = "Los Angeles";
         let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
         axios.get(apiUrl).then(handleResponse);
-
-        return "Loading..."
-    
     }
+
+if (weatherData.ready) {
+  return (
+    <div className="Weather">
+      <form onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+              autoFocus="on"
+              onChange={handleCityChange}
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <WeatherInfo data={weatherData} />
+      <WeatherForecast coordinates={weatherData.coordinates} />
+    </div>
+  );
+} else {
+  search(); // Assuming search() is a function that fetches data
+  return <div>Loading...</div>;
 }
