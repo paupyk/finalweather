18de48d93aea813e2e9f9d2eb68f1fe498dc4134
Kaylea <kayleapaup@yahoo.com>
2024-07-23 18:52:24 -0500

    Corrected errors and use of correct url

--- src/Weather.js
+++ src/Weather.js
@@ -3,10 +3,10 @@ import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
 import axios from "axios";
 
-function Weather() {
+function Weather(props) {
   // Define your state variables using the useState hook
-  const [weatherData, setWeatherData] = useState({ready: false});
-  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   // Define the necessary functions
   function handleResponse(response) {
@@ -19,7 +19,7 @@ function Weather() {
       description: response.data.weather[0].description,
       wind: response.data.wind.speed,
       city: response.data.name,
-      iconURL: "https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+      iconURL: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
 
@@ -39,31 +39,36 @@ function Weather() {
   }
 
   // Return JSX element within the component
-  return (
-    <div className="Weather">
-      <form onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city.."
-              className="form-control"
-              autoFocus="on"
-              onChange={handleCityChange}
-            />
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <WeatherInfo data={weatherData} />
-    </div>
-  );
+        </form>
+        <WeatherInfo data={weatherData} />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
 
 export default Weather;
