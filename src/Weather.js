dd4e207a7df27bed637d48bdda9de8fa5be62288
Kaylea <kayleapaup@yahoo.com>
2024-08-05 19:35:28 -0500

    Edited apiUrl and edited footer

--- src/Weather.js
+++ src/Weather.js
@@ -33,7 +33,7 @@ function Weather(props) {
 
   function search() {
     const apiKey = "4o0269f4b7t3d5f7f0cfc4a0af394b27";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
     axios.get(apiUrl).then(handleResponse);
   }
 
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,6 +16,11 @@ function handleResponse(response) {
         setForecast(response.data.daily);
         setLoaded(true);
     }
+
+    function load() {
+    const apiKey = "4o0269f4b7t3d5f7f0cfc4a0af394b27";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${props.coordinates.longitude}&lat=${props.coordinates.latitude}&key=${apiKey}&units=imperial`;
+    }
     if (loaded) {
         console.log(props);
         return (
